8401780d480b0853e93a76affb8c23abbb83e7f0
JoeShmoe9 <106446252+JoeShmoe9@users.noreply.github.com>
2022-07-05 14:18:30 -0500

    Update keylog.bat

--- keylog.bat
+++ keylog.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\jseba\AppData\Local\Programs\Python\Python310\python.exe" "D:\Coding\Python\Danger\keylogger\keylogger.py"
+"path to python.exe" "path to the /keylogger.py"
 pause
